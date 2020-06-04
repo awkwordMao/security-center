b46c2b2782de63929a680934948126996efc8ed8
awkwordMao <2624824150@qq.com>
2020-06-04 09:04:06 +0800

    Initial commit

---
+++ src/main/java/com/lzw/securitycenter/SecuritycenterApplication.java
@@ -0,0 +1,13 @@
+package com.lzw.securitycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritycenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lzw/securitycenter/SecuritycenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzw.securitycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritycenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
